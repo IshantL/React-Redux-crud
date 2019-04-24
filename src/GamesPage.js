4a761b9d463a55a1fa6e8756e3f4cedd443f2804
IshantL <ishantlambhate@gmail.com>
2019-04-24 03:27:09 +0000

    dispatch action

--- src/GamesPage.js
+++ src/GamesPage.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react';
 import {connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import GamesList from './GamesList';
+import {fetchGames} from './actions';
+
 class GamesPage extends Component{
+
+    componentDidMount(){
+        this.props.fetchGames();
+    }
 render(){
     return(
         <div>
@@ -14,11 +20,13 @@ render(){
 }
 
 GamesPage.propTypes={
-    games: PropTypes.array.isRequired
+    games: PropTypes.array.isRequired,
+    fetchGames: PropTypes.func.isRequired
 }
+
 function mapStateToProps(state){
     return {
         games: state.games
     }
 }
-export default connect(mapStateToProps)(GamesPage);
+export default connect(mapStateToProps,{fetchGames})(GamesPage);
